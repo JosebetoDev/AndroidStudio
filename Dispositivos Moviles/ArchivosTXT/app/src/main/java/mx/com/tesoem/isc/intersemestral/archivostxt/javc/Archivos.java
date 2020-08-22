7fa63f05a006726fd16ead5df6351e679c27241d
Jose Alberto V.C <jose_alberto_vc@hotmail.com>
2020-08-22 18:12:06 -0500

    Unidad 3 - Manejo de Archivos Android

---
+++ Dispositivos Moviles/ArchivosTXT/app/src/main/java/mx/com/tesoem/isc/intersemestral/archivostxt/javc/Archivos.java
@@ -0,0 +1,52 @@
+package mx.com.tesoem.isc.intersemestral.archivostxt.javc;
+
+import android.app.Activity;
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+
+public class Archivos {
+    public final String NomArch="Datos.txt";
+    public ArrayList<String> Datos = new ArrayList<>();
+    public boolean Grabar(Context context,ArrayList<String> Datos){
+        try {
+            OutputStreamWriter archivo = new OutputStreamWriter(context.openFileOutput(NomArch, Activity.MODE_PRIVATE));
+            for(String dato: Datos)
+                archivo.write(dato + "\n");
+            archivo.flush();
+            archivo.close();
+        }catch (Exception ex){
+            return false;
+        }
+        return true;
+    }
+    public boolean Leer(Context contexto){
+        try{
+            InputStreamReader archivo = new InputStreamReader(contexto.openFileInput(NomArch));
+            BufferedReader br = new BufferedReader(archivo);
+            String linea;
+            while((linea = br.readLine()) != null){
+                Datos.add(linea);
+            }
+        }catch (Exception ex){
+            return false;
+        }
+        return true;
+    }
+    public ArrayList<String> getDatos(){
+        return Datos;
+    }
+    public void Agregar(String dato,ArrayList<String> Datos){
+        this.Datos = Datos;
+        this.Datos.add(dato);
+    }
+    public boolean verifica(Context context){
+        String[] archivos = context.fileList();
+        for(String archivo:archivos)
+            if(archivo.equals(NomArch)) return true;
+            return false;
+    }
+}
